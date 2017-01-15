b9f0c96ccc2cff20da21d15f8140d99aaa93acfa
Victor <victormlndev@gmail.com>
2017-01-15 14:53:19 +0100

    Mejorado archivo update.sh

--- terminal/autopush.sh
+++ terminal/autopush.sh
@@ -34,19 +34,95 @@ source $( dirname "${BASH_SOURCE[0]}" )/user.conf
 echo "Easy Push v$version"
 if $show_author; then echo "Autor: Víctor Molina [victormln.com] <contact@victormln.com> "; fi;
 # Si están activadas las actualizaciones automáticas
-if $search_ota
+# Doy permiso al update.sh
+chmod +x $( dirname "${BASH_SOURCE[0]}" )/update.sh
+# Comprobaré si hay alguna versión nueva del programa autopush
+# y lo mostraré en pantalla
+source $( dirname "${BASH_SOURCE[0]}" )/update.sh
+
+# Compruebo que sistema está usando para hacer ping
+# Si es Linux o Mac
+if [ "$(uname -s)" == "Linux" ] || [ "$(uname)" == "Darwin" ]; then
+    # Do something under GNU/Linux platform
+    ping -c 1 www.google.com > /dev/null
+    has_internet=$(echo $?)
+    # Si es Windows
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ||
+  [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under Windows NT platform
+    ping -n 1 www.google.com > /dev/null
+    has_internet=$(echo $?)
+fi
+
+# Si el ping se ha realizado correctamente es que tiene internet
+# por lo que se buscaran actualizaciones
+if [ $has_internet -eq 0 ]
 then
-  # Doy permiso al update.sh
-  chmod +x $( dirname "${BASH_SOURCE[0]}" )/update.sh
-  # Comprobaré si hay alguna versión nueva del programa autopush
-  # y lo mostraré en pantalla
-  source $( dirname "${BASH_SOURCE[0]}" )/update.sh
-  # Si no tiene la ultima version y ha actualizado, volvemos a ejecutar el script
-  if ! $tieneUltimaVersion
+  if $search_ota
   then
-    # Iniciamos de nuevo el script para ejecutar el script actualizado
-    exec $( dirname "${BASH_SOURCE[0]}" )/autopush.sh
+    tieneUltimaVersion=false
+    # Conseguimos la ultima version que hay en github y le quitamos los puntos
+    ultimaVersion=$(curl -s https://raw.githubusercontent.com/victormln/easy-push/master/terminal/user.conf | tail -1 | cut -d'=' -f 2) > /dev/null
+    ultimaVersionSinPuntos=$( echo $ultimaVersion | tr -d ".")
+    # Miramos que versión tiene el usuario actualmente
+    versionActualSinPuntos=$(cat $( dirname "${BASH_SOURCE[0]}" )/user.conf | tail -1 | cut -d'=' -f 2 | tr -d ".")
+    # Comprobamos si la versionActual es igual o mas grande que la ultimaVersion
+    # es igual a la versionActual.
+    if (( $versionActualSinPuntos>=$ultimaVersionSinPuntos ))
+    then
+    	tieneUltimaVersion=true
+    else
+    	directorioActual=$(pwd)
+    	# Nos colocamos en el directorio del script, para actualizarlo
+    	cd "$( dirname "${BASH_SOURCE[0]}" )"
+    	cd ..
+    	# Mostramos el mensaje de que hay una nueva actualización
+    	echo "###########################################"
+    	echo -e "${WARNING}¡NUEVA ACTUALIZACIÓN!${NC}"
+    	echo "Tienes la versión: $version"
+    	echo "Versión disponible: $ultimaVersion"
+    	echo "###########################################"
+    	# Si tiene las actualizaciones automaticas, no se le pide nada
+    	if $automatic_update
+    	then
+    		# Si es así, hacemos un pull y le actualizamos el script
+    		echo "Hay una nueva actualización y tienes activadas las descargas automáticas."
+    		git pull | tee >(echo "Actualizando... Por favor, espere ...")
+    		echo -e "${OK}[OK] ${NC}La actualización ha acabado, por favor, vuelva a iniciar el script.";
+    	else
+    	  echo "Hay una nueva versión de este script y se recomienda actualizar."
+    	  echo "Quieres descargarla y así tener las últimas mejoras? y/n o s/n"
+    	  # Preguntamos si quiere actualizar
+    	  read actualizar
+    	  if [ $actualizar == "s" ] || [ $actualizar == "y" ]
+    	  then
+    			directorioActual=$(pwd)
+    			cd "$( dirname "${BASH_SOURCE[0]}" )"
+    			cd ..
+    	    # Si es así, hacemos un pull y le actualizamos el script
+    	  	git pull | tee >(echo "Actualizando... Por favor, espere ...")
+    			cd $directorioActual
+    			echo -e "${OK}[OK] ${NC}La actualización ha acabado, por favor, vuelva a iniciar el script.";
+    	  else
+    	    # En el caso que seleccione que no, muestro un mensaje.
+    	    echo -e "${WARNING}¡AVISO!${NC} NO se actualizará (aunque se recomienda)."
+    			# Damos por su puesto que tiene la ultima version,
+    			# para que el script no entre en bucle
+    			tieneUltimaVersion=true
+    	  fi
+    	fi
+    	# Cambiamos al directorio donde el usuario tiene sus cambios
+    	cd $directorioActual
+    fi
+    # Si no tiene la ultima version y ha actualizado, volvemos a ejecutar el script
+    if ! $tieneUltimaVersion
+    then
+      # Iniciamos de nuevo el script para ejecutar el script actualizado
+      exec $( dirname "${BASH_SOURCE[0]}" )/autopush.sh
+    fi
   fi
+else
+  echo -e "${WARNING}[AVISO] ${NC}No tienes internet. Para buscar actualizaciones se necesita internet."
 fi
 
 # Comprobamos si existe un .git, si no existe, mostraremos un mensaje y saldremos

d4e0a2681831b67cdde2937fe124cbe3f15b25c3
Victor <victormlndev@gmail.com>
2017-01-31 21:55:56 +0100

    MMejorado install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # Hace una instalación del easy-push en modo gráfico y en modo terminal
 
+# Mensajes de color
+ERROR='\033[0;31m'
+OK='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 # Creo un alias para el modo grafico y modo terminal
 actualDir=$(pwd)
 echo "alias gp=\"$actualDir/terminal/autopush.sh\"" >> ~/.bashrc
 echo "alias gpg=\"$actualDir/graphic/autopush.sh\"" >> ~/.bashrc
+echo -e "${OK}[OK]${NC} Instalación finalizada."
+echo "Reinicie este terminal y ejecute ${BLUE}gp -v${NC} para comprobar que se ha instalado correctamente."
+exit
